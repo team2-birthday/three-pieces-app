06e26669c5d31d4e1e2ababaef5ac4122935f16e
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-02-25 15:07:36 +0900

    Vue パレット一通り完成

--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -1,13 +1,54 @@
 <template>
   <h1>Vue パレット</h1>
   <div class="app">
-    <div class="palette" style="background-color: rgba(0, 0, 200, 0.5)"></div>
-    <p>rgba( {{ 0 }}, {{ 0 }}, 200, 0.5 )</p>
+    <div 
+      class="palette" 
+      v-bind:style="{ backgroundColor: `rgba(${red}, ${green}, ${blue}, 0.5)` }" 
+      v-on:click="colorDecision"
+      v-on:mousemove="colorChange"
+    >
+    </div>
+    <p>rgba( {{ red }}, {{ green }}, {{ blue }}, 0.5 )</p>
+    <div class="red">赤</div>
+    <input type="range" min="0" max="255" v-model="red">
+    <div class="green">緑</div>
+    <input type="range" min="0" max="255" v-model="green">
+    <div class="blue">青</div>
+    <input type="range" min="0" max="255" v-model="blue">
+    <button v-on:click="colorDecision" class="color-decision">この色に決定</button>
     <div class="colors-container">
-      <div class="mini-palette"></div>
+      <div 
+        v-for="color in colors" v-bind:key="color" v-bind:color="color" 
+        v-bind:style="{ backgroundColor: `rgba(${color.red}, ${color.green}, ${color.blue}, 0.5)` }"
+        class="mini-palette"
+      >
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+  export default{
+    data(){
+      return{
+        red: 0,
+        green: 0,
+        blue: 0,
+        colors: []
+      }
+    },
+    methods: {
+      colorDecision: function(){
+        this.colors.push({red: this.red, green: this.green, blue: this.blue})
+      },
+      colorChange: function(e){
+        this.red=e.offsetX
+        this.green=e.offsetY
+      }
+    },
+  }
+
+</script>
 
 <style>
 .app {
@@ -19,6 +60,18 @@
 .palette {
   width: 255px;
   height: 255px;
+}
+.red{
+  color: red;
+}
+.green{
+  color: green;
+}
+.blue{
+  color: blue;
+}
+.color-decision{
+  margin-top: 1%;
 }
 .mini-palette {
   min-width: 60px;
