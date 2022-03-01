267d54984e2c0f4ac8ac92463eea9f45c8705c4b
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-03-01 09:32:00 +0900

    透明化の変数名と初期値の変更と文字のデザイン変更

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,83 @@
 <template>
   <h1>Home</h1>
   <nav class="navigation">
-      <router-link
-        to="/quiz"
-        exact-active-class="navigation__link--exact-active"
-        v-on:mouseover="overQuiz"
-        v-on:mouseout="outQuiz"
-        v-bind:class="{ judgequiz: judgeQuiz, navigation__link: navigationLink.quiz}"
-        >Vue クイズ</router-link
-      >|
-      <router-link
-        to="/palette"
-        exact-active-class="navigation__link--exact-active"
-        v-on:mouseover="overPalette"
-        v-on:mouseout="outPalette"
-        v-bind:class="{ judgepalette: judgePalette, navigation__link: navigationLink.palette}"
-        >Vue パレット</router-link
-      >|
-      <router-link
-        to="/memo"
-        exact-active-class="navigation__link--exact-active"
-        v-on:mouseover="overMemo"
-        v-on:mouseout="outMemo"
-        v-bind:class="{ judgememo: judgeMemo, navigation__link: navigationLink.memo}"
-        >Vue メモ</router-link
-      >
-    </nav>
+    <router-link
+      to="/quiz"
+      exact-active-class="navigation__link--exact-active"
+      v-on:mouseover="overQuiz"
+      v-on:mouseout="outQuiz"
+      v-bind:class="{
+        judgequiz: judgeQuiz,
+        navigation__link: navigationLink.quiz,
+      }"
+      >Vue クイズ</router-link
+    >|
+    <router-link
+      to="/palette"
+      exact-active-class="navigation__link--exact-active"
+      v-on:mouseover="overPalette"
+      v-on:mouseout="outPalette"
+      v-bind:class="{
+        judgepalette: judgePalette,
+        navigation__link: navigationLink.palette,
+      }"
+      >Vue パレット</router-link
+    >|
+    <router-link
+      to="/memo"
+      exact-active-class="navigation__link--exact-active"
+      v-on:mouseover="overMemo"
+      v-on:mouseout="outMemo"
+      v-bind:class="{
+        judgememo: judgeMemo,
+        navigation__link: navigationLink.memo,
+      }"
+      >Vue メモ</router-link
+    >
+  </nav>
 </template>
 
 <script>
-  export default{
-    data(){
-      return{
-        judgeQuiz: false,
-        judgePalette: false,
-        judgeMemo: false,
-        navigationLink:{
-          quiz: true,
-          palette: true,
-          memo: true
-        },
-      }
+export default {
+  data() {
+    return {
+      judgeQuiz: false,
+      judgePalette: false,
+      judgeMemo: false,
+      navigationLink: {
+        quiz: true,
+        palette: true,
+        memo: true,
+      },
+    }
+  },
+  methods: {
+    overQuiz: function () {
+      this.judgeQuiz = true
+      this.navigationLink.quiz = false
     },
-    methods: {
-      overQuiz: function(){
-        this.judgeQuiz=true
-        this.navigationLink.quiz=false
-      },
-      outQuiz:function(){
-        this.judgeQuiz=false
-        this.navigationLink.quiz=true
-      },
-      overPalette: function(){
-        this.judgePalette=true
-        this.navigationLink.palette=false
-      },
-      outPalette:function(){
-        this.judgePalette=false
-        this.navigationLink.palette=true
-      },
-      overMemo: function(){
-        this.judgeMemo=true
-        this.navigationLink.memo=false
-      },
-      outMemo:function(){
-        this.judgeMemo=false
-        this.navigationLink.memo=true
-      },
+    outQuiz: function () {
+      this.judgeQuiz = false
+      this.navigationLink.quiz = true
     },
-  }
+    overPalette: function () {
+      this.judgePalette = true
+      this.navigationLink.palette = false
+    },
+    outPalette: function () {
+      this.judgePalette = false
+      this.navigationLink.palette = true
+    },
+    overMemo: function () {
+      this.judgeMemo = true
+      this.navigationLink.memo = false
+    },
+    outMemo: function () {
+      this.judgeMemo = false
+      this.navigationLink.memo = true
+    },
+  },
+}
 </script>
 
 <style>
@@ -80,19 +89,19 @@
   font-size: 1.5rem;
 }
 
-.judgequiz{
+.judgequiz {
   color: blue;
   font-size: 2rem;
   font-weight: bold;
 }
 
-.judgepalette{
+.judgepalette {
   color: blue;
   font-size: 2rem;
   font-weight: bold;
 }
 
-.judgememo{
+.judgememo {
   color: blue;
   font-size: 2rem;
   font-weight: bold;
@@ -102,5 +111,4 @@
   font-weight: bold;
   color: #2c3e50;
 }
-
 </style>
--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -1,66 +1,76 @@
 <template>
   <h1>Vue パレット</h1>
   <div class="app">
-    <div 
-      class="palette" 
-      v-bind:style="{ backgroundColor: `rgba(${red}, ${green}, ${blue}, ${transparency})` }" 
+    <div
+      class="palette"
+      v-bind:style="{
+        backgroundColor: `rgba(${red}, ${green}, ${blue}, ${opacity})`,
+      }"
       v-on:click="colorDecision"
       v-on:mousemove="colorChange"
-    >
-    </div>
-    <p>rgba( {{ red }}, {{ green }}, {{ blue }}, {{transparency}} )</p>
+    ></div>
+    <p>rgba( {{ red }}, {{ green }}, {{ blue }}, {{ opacity }} )</p>
     <!-- rangeで色を指定できるようにした -->
     <div class="red">赤</div>
-    <input type="range" min="0" max="255" v-model="red">
+    <input type="range" min="0" max="255" v-model="red" />
     <div class="green">緑</div>
-    <input type="range" min="0" max="255" v-model="green">
+    <input type="range" min="0" max="255" v-model="green" />
     <div class="blue">青</div>
-    <input type="range" min="0" max="255" v-model="blue">
-    <div class="transparency">透明度</div>
-    <input type="range" min="0" max="1" step="0.01" v-model="transparency">
-    <button v-on:click="colorDecision" class="color-decision">この色に決定</button>
+    <input type="range" min="0" max="255" v-model="blue" />
+    <div class="opacity">透明度</div>
+    <input type="range" min="0" max="1" step="0.01" v-model="opacity" />
+    <button v-on:click="colorDecision" class="color-decision">
+      この色に決定
+    </button>
     <div class="colors-container">
-      <div 
-        v-for="color in colors" v-bind:key="color" v-bind:color="color" 
-        v-bind:style="{ backgroundColor: `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.transparency})` }"
+      <div
+        v-for="color in colors"
+        v-bind:key="color"
+        v-bind:color="color"
+        v-bind:style="{
+          backgroundColor: `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.opacity})`,
+        }"
         v-on:click="pastColorDecision(color)"
         class="mini-palette"
-      >
-      </div>
+      ></div>
     </div>
   </div>
 </template>
 
 <script>
-  export default{
-    data(){
-      return{
-        red: 0,
-        green: 0,
-        blue: 0,
-        transparency: 0,
-        colors: []
-      }
+export default {
+  data() {
+    return {
+      red: 0,
+      green: 0,
+      blue: 0,
+      opacity: 1,
+      colors: [],
+    }
+  },
+  methods: {
+    colorDecision: function () {
+      this.colors.push({
+        red: this.red,
+        green: this.green,
+        blue: this.blue,
+        opacity: this.opacity,
+      })
     },
-    methods: {
-      colorDecision: function(){
-        this.colors.push({red: this.red, green: this.green, blue: this.blue, transparency: this.transparency})
-      },
-      //paletteの位置でX座標で赤,Y座標で緑が変わる
-      colorChange: function(e){
-        this.red=e.offsetX
-        this.green=e.offsetY
-      },
-      // クリックしたミニパレットの色をパレットに表示する
-      pastColorDecision: function(color){
-        this.red=color.red
-        this.green=color.green
-        this.blue=color.blue
-        this.transparency=color.transparency
-      }
+    //paletteの位置でX座標で赤,Y座標で緑が変わる
+    colorChange: function (e) {
+      this.red = e.offsetX
+      this.green = e.offsetY
     },
-  }
-
+    // クリックしたミニパレットの色をパレットに表示する
+    pastColorDecision: function (color) {
+      this.red = color.red
+      this.green = color.green
+      this.blue = color.blue
+      this.opacity = color.opacity
+    },
+  },
+}
 </script>
 
 <style>
@@ -74,34 +84,63 @@
   width: 255px;
   height: 255px;
 }
-.red{
-  font-size:1.5em;
-	text-align:center;
-	line-height:0.95em;
-	font-weight:bold;
-	color: transparent;
-	background: repeating-linear-gradient(0deg, #C62828 0.1em, #EF5350 0.2em, #FFEBEE 0.3em, #EF5350 0.4em, #C62828 0.5em); 
-	-webkit-background-clip: text;
+.red {
+  font-size: 1.5em;
+  text-align: center;
+  line-height: 0.95em;
+  font-weight: bold;
+  color: transparent;
+  background: repeating-linear-gradient(
+    0deg,
+    #c62828 0.1em,
+    #ef5350 0.2em,
+    #ffebee 0.3em,
+    #ef5350 0.4em,
+    #c62828 0.5em
+  );
+  -webkit-background-clip: text;
 }
-.green{
-  font-size:1.5em;
-	text-align:center;
-	line-height:0.95em;
-	font-weight:bold;
-	color: transparent;
-	background: repeating-linear-gradient(0deg, #28c635 0.1em, #3fdf54 0.2em, #FFEBEE 0.3em, #50ef85 0.4em, #28c628 0.5em); 
-	-webkit-background-clip: text;
+.green {
+  font-size: 1.5em;
+  text-align: center;
+  line-height: 0.95em;
+  font-weight: bold;
+  color: transparent;
+  background: repeating-linear-gradient(
+    0deg,
+    #28c635 0.1em,
+    #3fdf54 0.2em,
+    #ffebee 0.3em,
+    #50ef85 0.4em,
+    #28c628 0.5em
+  );
+  -webkit-background-clip: text;
 }
-.blue{
-  font-size:1.5em;
-	text-align:center;
-	line-height:0.95em;
-	font-weight:bold;
-	color: transparent;
-	background: repeating-linear-gradient(0deg, #0277BD 0.1em, #4FC3F7 0.2em, #E1F5FE 0.3em, #4FC3F7 0.4em, #0277BD 0.5em); 
-	-webkit-background-clip: text;
+.blue {
+  font-size: 1.5em;
+  text-align: center;
+  line-height: 0.95em;
+  font-weight: bold;
+  color: transparent;
+  background: repeating-linear-gradient(
+    0deg,
+    #0277bd 0.1em,
+    #4fc3f7 0.2em,
+    #e1f5fe 0.3em,
+    #4fc3f7 0.4em,
+    #0277bd 0.5em
+  );
+  -webkit-background-clip: text;
 }
-.color-decision{
+.opacity {
+  font-size: 1.5em;
+  text-align: center;
+  line-height: 0.95em;
+  font-weight: bold;
+  color: #fff;
+  text-shadow: 0 0 0.2em rgba(0, 0, 0, 1);
+}
+.color-decision {
   margin-top: 1%;
   position: relative;
   display: inline-block;
@@ -113,12 +152,12 @@
   background: #24242c;
   font-size: 20px;
   color: rgb(6, 241, 131);
-  transition: .4s;
+  transition: 0.4s;
   border: none;
 }
-.color-decision:before{
+.color-decision:before {
   position: absolute;
-  content: '';
+  content: "";
   left: 0;
   top: 0;
   width: 0;
@@ -127,11 +166,11 @@
   border-left: solid 21px white;
   border-bottom: solid 41px transparent;
   z-index: 1;
-  transition: .4s;
+  transition: 0.4s;
 }
-.color-decision:after{
+.color-decision:after {
   position: absolute;
-  content: '';
+  content: "";
   right: 0;
   top: 0;
   width: 0;
@@ -140,9 +179,10 @@
   border-left: solid 21px transparent;
   border-bottom: solid 41px white;
   z-index: 1;
-  transition: .4s;
+  transition: 0.4s;
 }
-.color-decision:hover:before, .color-decision:hover:after {
+.color-decision:hover:before,
+.color-decision:hover:after {
   border-left-width: 25px;
 }
 .mini-palette {
